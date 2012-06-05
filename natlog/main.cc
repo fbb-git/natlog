72d4edae30d5c0219f57704f7feafd56315aabbd
Frank B. Brokken <f.b.brokken@rug.nl>
2012-06-05 16:22:27 +0200

    Initial running conntrack program

--- natlog/main.cc
+++ natlog/main.cc
@@ -1,10 +1,102 @@
 #include "main.ih"
 
 // Room for Args initialization
+namespace   // the anonymous namespace can be used here
+{
+    Arg::LongOption longOptions[] =
+    {
+        Arg::LongOption("help", 'h'),
+        Arg::LongOption("version", 'v'),
+        Arg::LongOption("daemon", 'd'),
+        Arg::LongOption("conntrack", Arg::Required),
+    };
+
+    Arg::LongOption const *const longEnd =
+                    longOptions + 
+                    sizeof(longOptions) / sizeof(longOptions[0]);
+}
+
+char const g_config[] = "natlog.conf";
 
 int main(int argc, char **argv)
 try
 {
+    ArgConfig &arg = ArgConfig::initialize("c:dhv", 
+                        longOptions, longEnd, argc, argv);
+    
+    arg.versionHelp(usage, Icmbuild::version, 1);
+
+    string config = g_config;
+    arg.option(&config, 'c');
+
+    if (Stat(config))
+        arg.open(config);
+
+    if (string(arg[0]) != "conntrack")
+        fmsg << "Currently only `conntrack' is available" << endl;
+
+    string cmd;
+    if (not arg.option(&cmd, "conntrack"))
+    {
+        if (not Stat(config))
+            fmsg << "--contrack option missing" << endl; 
+        else
+            fmsg << "no --contrack option and no conntrack spec. "
+                    "found in " << config << endl;
+    }
+    //cout << "conntrack command: " << cmd << '\n';
+
+
+    Process conntrack(Process::COUT, cmd);
+
+    conntrack.start();
+
+    string line;
+    Pattern pat(
+    //     1        2      3
+    "\\[(\\d+)\\.(\\d+).*(NEW|DESTROY).*"          // time: [1338899277.41469 ]
+    //      4             5
+    "src=(\\S+)\\s+dst=(\\S+)\\s+"          // source to nat,  dest
+    //        6               7
+    "sport=(\\d+)\\s+dport=(\\d+).*"        // source from,    dest port
+    //        8
+    "dport=(\\d+)");                        // natted sport
+
+
+    struct Record
+    {
+        string time1;
+        string time2;
+    };
+
+    unordered_map<string, Record> record;
+
+    while (getline(conntrack, line))
+    {
+        if (pat << line)
+        {
+            if (pat[3] == "NEW")
+                record[pat[8]] = Record {pat[1], pat[2]};
+            else 
+            {
+                auto iter = record.find(pat[8]);
+                if (iter == record.end())
+                    cout << "UNAVAILABLE: " << line << '\n';
+                else
+                    cout << "From " << iter->second.time1 << ':' << 
+                                                    iter->second.time2 << 
+                            " until " << pat[1] << ':' << pat[2] << ":\n"
+                        "    " << pat[4] << ':' << pat[6] << 
+                                    " (via: " << pat[8] << ") "
+                        "to " << pat[5] << ':' << pat[7] << '\n';
+            }
+        }
+    }
+}
+catch (Errno const &err)
+{
+    cout << err.why() << '\n';
+    return 1;
 }
 catch (...)
 {
--- natlog/usage.cc
+++ natlog/usage.cc
@@ -8,11 +8,16 @@ void usage(std::string const &progname)
     progname << " by " << Icmbuild::author << "\n" <<
     progname << " V" << Icmbuild::version << " " << Icmbuild::years << "\n"
     "\n"
-    "Usage: " << progname << " [options] args\n"
+    "Usage: " << progname << " [options] command\n"
     "Where:\n"
     "   [options] - optional arguments (short options between parentheses):\n"
-    "      --help (-h)      - provide this help\n"
-    "      --version (-v)   - show version information and terminate\n"
-    "   args   - explain additional arguments.\n"
+    "      --config (-c) path - path: pathname of the configuation file\n"
+    "                           (default: `" << g_config << "'\n"
+    "      --conntrack 'cmd'  - cmd: conntrack command to execute\n"
+    "      --daemon (-d)      - run " << progname << " in the background\n"
+    "      --help (-h)        - provide this help\n"
+    "      --version (-v)     - show version information and terminate\n"
+    "   command   - command to execute:\n"
+    "       conntrack:  use `conntrack' to find the source-nat connections\n"
     "\n";
 }
