463eb55a94a0d3c693c4f56c39074a8a87a50e00
Frank B. Brokken <f.b.brokken@rug.nl>
2013-12-19 15:25:31 +0100

    Checked conntrack/signalhandler.cc for incoming signals 15 and 9

--- natlog/conntrack/signalhandler.cc
+++ natlog/conntrack/signalhandler.cc
@@ -2,6 +2,9 @@
 
 void Conntrack::signalHandler(size_t signum)
 {
+    if (signum != SIGTERM && signum != SIGKILL)
+        return;
+
     d_stdMsg << "Stopping natlog on receiving signal " << signum << "\n"
                 "Ending conntrack having pid " << pid() << endl;
 
