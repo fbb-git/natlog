99ad09e2d5a647e4b07ad7e4ddd77d2fc696acbc
Frank B. Brokken <f.b.brokken@rug.nl>
2018-03-06 21:10:05 +0100

    cosmetics

--- natlog/tcp/cleanuphook.cc
+++ natlog/tcp/cleanuphook.cc
@@ -1,5 +1,10 @@
 #include "tcp.ih"
 
+namespace {
+    size_t a_udpSize = 50;
+    size_t a_recordCount = 50;
+}
+
 void TCP::cleanupHook()
 {
     vector<size_t> remove;
@@ -13,8 +18,19 @@ void TCP::cleanupHook()
     for (size_t key: remove)
         d_keyMap.erase(d_keyMap.find(key));
 
-    if (g_debug.good() and size() % 100 == 0)
-        g_debug << "TCP size: " << size() << 
+    if (not g_debug.good())
+        return;
+
+    bool newSize =  size() > a_udpSize;
+    if (newSize)
+        a_udpSize = size() + 50;
+
+    bool newCount = Record::count() > a_recordCount;
+    if (newCount)
+        a_recordCount = Record::count() + 50;
+
+    if (newCount or newSize)
+        g_debug << "UDP size: " << size() << 
                     ", keyMap size: " << d_keyMap.size() <<
-                    ", #records: " << Record::count() << '\n';
+                    ", #records: " << Record::count() << endl;
 }
