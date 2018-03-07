4d071a65498de934011b5b98ac731feea9c02441
Frank B. Brokken <f.b.brokken@rug.nl>
2018-03-07 14:53:03 +0100

    wip

--- natlog/tcp/syn.cc
+++ natlog/tcp/syn.cc
@@ -5,7 +5,7 @@ void TCP::syn(RecordPtr &next)
     if (                                            // package sent
         g_nic.mask(Record::IN, next->sourceIP())
         &&                                          // and not a local
-        g_nic.mask(Record::IN, next->destIP())      // connection
+        not g_nic.mask(Record::IN, next->destIP())  // connection
     )
     {
         next->setSrcKey();                  // store the outDev key
